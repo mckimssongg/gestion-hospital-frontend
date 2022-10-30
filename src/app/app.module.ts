2c76be171b626310c5cc2a97cec72cc9cd1f7cdf
DuceHerrera07 <95140708+DuceHerrera07@users.noreply.github.com>
2022-10-29 19:36:16 -0600

    primeros cambios para login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 
+import { LoginComponent } from './components/login/login.component';
+
+//ANGULAR MATERIAL
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     StoreModule.forRoot({}, {}),
+<<<<<<< Updated upstream
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+=======
+    BrowserAnimationsModule,
+
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
