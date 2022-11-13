d43efceade9fdcd372a23d8fbed2309d03f533ab
mckimssongg <soymckimsson286@gmail.com>
2022-11-13 14:35:59 -0600

    add

--- src/app/services/especialidades.service.ts
+++ src/app/services/especialidades.service.ts
@@ -16,4 +16,28 @@ export class EmpleadoService {
   //Url de la API que obtiene el listado de los empleados
   private baseUrl = 'http://localhost:8080/';
   constructor(private httpClient: HttpClient) {}
+
+  /**
+   * @returns Observable<Especialidades[]>
+   * @description Obtiene el listado de especialidades
+   */
+  obtenerLista(): Observable<Especialidades[]> {
+    return this.httpClient.get<Especialidades[]>(
+      `${this.baseUrl} ` + `especialidades`
+    );
+  }
+
+  /**
+   * @param empleado Especialidades
+   * @returns Observable<Object>
+   * @description Crea un nuevo empleado
+   * @example
+   * {
+   * id_especialidad: number,
+   * nombre: string
+   * }
+   */
+  registrar(obj: Especialidades): Observable<Object> {
+    return this.httpClient.post(`${this.baseUrl}`, obj);
+  }
 }
