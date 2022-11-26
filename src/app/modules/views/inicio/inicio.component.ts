1cba75ec59156207f0aa0d1e32949a7c06039ea0
mckimssongg <soymckimsson286@gmail.com>
2022-11-25 23:01:08 -0600

    fix inicio

--- src/app/modules/views/inicio/inicio.component.ts
+++ src/app/modules/views/inicio/inicio.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+//import chart.js
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
